bb735a2fb5cdc3b724546c21c01cbcb04034c601
Matt McGrattan <matt.mcgrattan@digirati.com>
2022-02-15 12:18:02 +0000

    Indexing of language map fields into indexables

--- text_store/serializers.py
+++ text_store/serializers.py
@@ -4,6 +4,7 @@ from rest_framework.validators import UniqueValidator
 from django.contrib.contenttypes.models import ContentType
 from bs4 import BeautifulSoup
 import bleach
+from django.utils.translation import get_language
 
 from search_service.serializers import (
     BaseModelToIndexableSerializer,
@@ -16,6 +17,8 @@ from .models import (
 )
 
 logger = logging.getLogger(__name__)
+
+default_lang = get_language()
 
 
 class TextResourceCreateSerializer(serializers.ModelSerializer):
@@ -96,13 +99,79 @@ class LanguageMapToIndexablesSerializer(BaseModelToIndexableSerializer):
 
 
 class TextResourceToIndexableSerializer(BaseModelToIndexableSerializer):
+    indexable_text_fields = [
+        {"key": "label", "indexable_type": "text", "index_as": "text"},
+        {"key": "text_title", "indexable_type": "text", "index_as": "text"},
+        {"key": "text_subtitle", "indexable_type": "text", "index_as": "text"},
+    ]
+
+    def _text_indexable(
+        self,
+        type,
+        subtype,
+        value,
+        language,
+    ):
+        return {
+            "type": type,
+            "subtype": subtype.lower(),
+            "indexable_text": BeautifulSoup(value, "html.parser").text,
+            "original_content": str({subtype: bleach.clean(value)}),
+            "language": language,
+        }
+
+    def _normalise_field(self, field_data):
+        if isinstance(field_data, dict):
+            return [field_data]
+        elif isinstance(field_data, list):
+            return field_data
+        else:
+            return [{"none": field_data}]
+
+    def _normalise_language(self, language):
+        if language in ["@none", "none"]:
+            return default_lang
+        else:
+            return language
+
+    def _indexables_from_field(
+        self,
+        field_instance,
+        key=None,
+        indexable_type="descriptive",
+        index_as="text",
+    ):
+        indexables = []
+        for val_lang, vals in field_instance.items():
+            lang = self._normalise_language(val_lang)
+            if vals:
+                for str_value in map(str, vals):
+                    if index_as == "text":
+                        indexables.append(
+                            self._text_indexable(
+                                type=indexable_type,
+                                subtype=key,
+                                value=str_value,
+                                language=lang,
+                            )
+                        )
+        return indexables
+
     def to_indexables(self, instance):
-        return [
+        indexables = [
             {
                 "type": "text",
                 "subtype": instance.text_type,
                 "original_content": bleach.clean(instance.text_content),
                 "indexable_text": BeautifulSoup(instance.text_content, "html.parser").text,
                 "language": instance.language,
-            }
+            },
         ]
+        for field_lookup in self.indexable_text_fields:
+            if field := getattr(instance, field_lookup.get("key")):
+                norm_field = self._normalise_field(field)
+                for field_instance in norm_field:
+                    indexables.extend(
+                        self._indexables_from_field(field_instance, **field_lookup)
+                    )
+        return indexables
